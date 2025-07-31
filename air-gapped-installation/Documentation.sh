8b205b659c4c33b5c2840fde96e7ffe2a59a272c
Nandan Hebbar <nandan@platformatory.com>
2025-07-31 11:14:49 +0530

    Update Documentation.sh

--- air-gapped-installation/Documentation.sh
+++ air-gapped-installation/Documentation.sh
@@ -1,142 +1,118 @@
-multipass launch --name VMname
-
-multipass shell ControlNode
-
-multipass info ControlNode
-
-uname -a
-
-multipass list
-
-multipass info
+-- This explains the simulation of air gapped installation of the Confluent Platform with CP ansible on your local machine using the multipass VM
 
 
+    Create 2 VMs 
+    
+    1. ControlNode
+    multipass launch --name ControlNode --cpus 4 --mem 4G --disk 25G 20.04
+    
+    2. HostNode
+    multipass launch --name HostNode --cpus 4 --mem 8G --disk 75G 20.04
 
+    3. On HostNode disableinternet connectivity by
+    sudo ip route del default
+    sudo sed -i '/^nameserver/d' /etc/resolv.conf
+    
+    4. check python version on both nodes (refer the CP documentation for the versioning)
+    python3 --version 
+    
+    Upgrade the python version to 3.12
 
-Create 2 VMs 
+        sudo apt update
+        sudo apt install python3.12
+        python3 --version
+    
+    5. Install ansible on ControlNode
+        pip install ansible==9.4.0
+    
+    
+    6. download CP package from ansible galaxy using on the ControlNode
+    
+        pip install --upgrade packaging
+        python3 -m pip show packaging
+        ansible-galaxy collection install confluent.platform
 
-1. ControlNode
-multipass launch --name ControlNode --cpus 4 --mem 4G --disk 25G 20.04
-
-2. HostNode
-multipass launch --name HostNode --cpus 4 --mem 8G --disk 75G 20.04
-
-a. On HostNode disableinternet connectivity by
-sudo ip route del default
-sudo sed -i '/^nameserver/d' /etc/resolv.conf
-
-b. check python version
-python3 --version 
-
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt update
-    sudo apt install python3.12
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
-    sudo update-alternatives --config python3
-    python3 --version
-
-c. Install ansible
-    pip install ansible==9.4.0
-
-
-source ansible-venv/bin/activate	
-
-d. download CP package from ansible galaxy using
-
-    pip install --upgrade packaging
-    python3 -m pip show packaging
-    ansible-galaxy collection install confluent.platform
-
-Installing 'confluent.platform:7.9.0' to '/home/ubuntu/.ansible/collections/ansible_collections/confluent/platform'
-
-
-e. download the cryptographic package from the ControlNode and move it to the CP-Host VM
-
-f. On the ControlNode download the package 
-    pip download cryptography
-
-g. It will download 3 files, now move it to the CP-Host using the below command 
-    multipass transfer /home/nandan/air-gapped-installation/cryptography-44.0.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl CP-Host:/home/ubuntu/
+    
+    
+    7. download the cryptographic package from the ControlNode and move it to the CP-Host VM
+        pip download cryptography
+    
+        It will download 3 files, now move it to the CP-Host using the below command 
+        multipass transfer /home/nandan/air-gapped-installation/cryptography-44.0.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl CP-Host:/home/ubuntu/
 
 
  
-Install them
-
-sudo apt update
-sudo apt install python3.12-venv
-python3 -m venv ~/venv-crypto
-source ~/venv-crypto/bin/activate
-pip install /home/ubuntu/cryptography-44.0.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-pip freeze | grep "cryptography"
-
+    8. Install them on the 
+        
+        sudo apt update
+        sudo apt install python3.12-venv
+        python3 -m venv ~/venv-crypto
+        source ~/venv-crypto/bin/activate
+        pip install /home/ubuntu/cryptography-44.0.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+        pip freeze | grep "cryptography"
 
 
 OR
 
-pip install /home/ubuntu/cryptography-44.0.2-*.whl
+        pip install /home/ubuntu/cryptography-44.0.2-*.whl
 
 
-Create ssh keys
-ssh-keygen -t rsa -b 4096
+    9. Create ssh keys on your localhost
+        ssh-keygen -t rsa -b 4096
+        
+        --Move ssh keys 
+        --Move the private key to the ControlNode
+            multipass transfer ~/cp-ssh ControlNode:/home/ubuntu/.ssh
 
-Move ssh keys 
-
-
-cp ~/.ssh/cp-ssh ~/Downloads/
-multipass transfer ~/Downloads/cp-ssh ControlNode:/home/ubuntu/.ssh
-
-cp ~/.ssh/cp-ssh ~/Downloads/
-multipass transfer ~/Downloads/cp-ssh.pub HostNode:/home/ubuntu/.ssh
-
-On host run
-    cat ~/.ssh/cp-ssh.pub >> ~/.ssh/authorized_keys
-
-    chmod 600 ~/.ssh/authorized_keys
-
-    chmod 700 ~/.ssh
-
-    chown ubuntu:ubuntu ~/.ssh ~/.ssh/authorized_keys
+        --Move the public key to the HostNode
+            multipass transfer ~/cp-ssh.pub HostNode:/home/ubuntu/.ssh
+        
+        --On host run
+            cat ~/.ssh/cp-ssh.pub >> ~/.ssh/authorized_keys
+        
+            chmod 600 ~/.ssh/authorized_keys
+        
+            chmod 700 ~/.ssh
+        
+            chown ubuntu:ubuntu ~/.ssh ~/.ssh/authorized_keys
 
 
 
-From ControlNode check the ssh connectivity
-
-    ssh -i ~/.ssh/cp-ssh ubuntu@10.85.60.35
-
-    ssh ubuntu@10.85.60.147
-
-
-On the distributed node download the CP installation package
-
-    curl -O http://packages.confluent.io/archive/7.9/confluent-7.9.0.tar.gz
-
-On the control node, extract the contents of the Confluent Platform archive:
-
-    tar xzf confluent-7.9.0.tar.gz
+    10. From ControlNode check the ssh connectivity
+        
+            ssh -i ~/.ssh/cp-ssh ubuntu@<Host-IP>
+        
+            ssh ubuntu@<Host-IP>
 
 
-Run readlink -f $(which java) to see the java path and version
+    11. On the distributed node download the CP installation package
+        
+            curl -O http://packages.confluent.io/archive/7.9/confluent-7.9.0.tar.gz
+
+    12. On the control node, extract the contents of the Confluent Platform archive:
+        
+            tar xzf confluent-7.9.0.tar.gz
 
 
-On the HostNode 
-    sudo mkdir -p /opt/confluent
-    sudo chown ubuntu:ubuntu /opt/confluent
-    sudo chmod -R 755 /opt/confluent
+    13. Run readlink -f $(which java) to see the java path and version
+
+
+    14. On the HostNode 
+        sudo mkdir -p /opt/confluent
+        sudo chown ubuntu:ubuntu /opt/confluent
+        sudo chmod -R 755 /opt/confluent
   
   
   
-Validate hosts before installing Confluent Platform:
+    15. Validate hosts before installing Confluent Platform:
+
+        check run the playbook
+            export ANSIBLE_HASH_BEHAVIOUR=merge
+            ansible-playbook -i host.yaml confluent.platform.all.yml --check -vvv
 
 
 
-
-check run the playbook
-    export ANSIBLE_HASH_BEHAVIOUR=merge
-    ansible-playbook -i host.yaml confluent.platform.all.yml --check -vvv
-
-
-
-
+Ansible Playbook:
 
 all:
   vars:
@@ -177,16 +153,14 @@ control_center:
     HostNode:
 
 
-
-
-
+--Post Installation checks
 
 See the installed services installed 
     cd /etc/systemd/system/
 
 
 Path of Bin directory to run the commands from
-    /opt/confluent/confluent-7.9.0/bin/kafka-topics
+    /opt/confluent/confluent-7.9.0/bin
 
 
 
@@ -236,6 +210,24 @@ kafka_connect:
 control_center:
   hosts:
     HostNode:
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
+-- Multipass commands
+
+-- Launch a new VM
+multipass launch --name VMname
+
+--To get into the shell of a VM
+multipass shell VMname
+
+--To get info about the VM
+multipass info VMname
+
+
+--To list all the VMs
+multipass list
 
 
 
